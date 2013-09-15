588c0d8477caac67adbc570126413931677b0b68
Tom Rice <t.rice90@gmail.com>
2013-09-15 01:37:06 -0400

    Intermediate commit. Wrote out the formula; something is yet broken.

--- set1/problem_set.py
+++ set1/problem_set.py
@@ -5,7 +5,8 @@ This is a script for the computational parts of Problem Set 1.
 
 from __future__ import division
 
-import scipy.constants
+import numpy as np
+#import scipy.constants
 import astropy.constants as const
 
 
@@ -22,10 +23,10 @@ def planck_function(effective_temperature, frequency):
 
     Parameters
     ----------
-    effective_temperature : float
+    effective_temperature : float (in units Kelvin)
         The effective temperature $T_{eff}$ of the blackbody 
-        under consideration.
-    frequency : float
+        under consideration. 
+    frequency : float (in units seconds^{-1})
         The frequency $\nu$ of the light being considered.
 
     Returns
@@ -37,7 +38,13 @@ def planck_function(effective_temperature, frequency):
 
     """
 
-    intensity = 2 * const.h * (frequency)**3 
+    h = const.h.to('J s')#.value # Planck's constant h, in Joules * seconds.
+    c = const.c.to('m/s')#.value # Speed of light c, in meters / seconds.
+    k_B = const.k_B.to('J/K')#.value # Boltzmann constant k_B, in Joules / kelvin.
+
+    intensity = ((2 * h * (frequency)**3) / c**2 / 
+                 (np.exp( (h * frequency) / (k_B * effective_temperature)) - 1)
+                 )
 
     return intensity
 
@@ -45,4 +52,7 @@ def planck_function(effective_temperature, frequency):
 def test_planck_function():
 
     assert 1 == 1 #placeholder
+
+    #    assert planck_function(6000, 2e12) == 
+    
     
