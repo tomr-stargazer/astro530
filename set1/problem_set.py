fe08b5b1651ed661e8de919d7f1e0819ddbe0506
Tom Rice <t.rice90@gmail.com>
2013-09-15 13:10:02 -0400

    Wrote a test for eddington temperature -- it passes.

--- set1/problem_set.py
+++ set1/problem_set.py
@@ -151,4 +151,12 @@ def test_planck_function():
     # See: http://wolfr.am/1ghoEBo for Wolfram Alpha example result
     assert round(planck_function(6000, 200e12), 8+3) == 2.985e-8
     
-    
+
+def test_eddington_temperature():
+
+    # Practically by definition, T(tau = 2/3) == T_eff
+    assert eddington_temperature(2/3, 6000) == 6000
+    assert eddington_temperature(2/3, 10) == 10
+
+    assert eddington_temperature(1, 6000) > 6000
+    assert eddington_temperature(1/2, 6000) < 6000
