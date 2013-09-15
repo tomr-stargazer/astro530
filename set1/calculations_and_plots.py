ec9769ce38f39f1796271e4082f2a227d9c0362f
Tom Rice <t.rice90@gmail.com>
2013-09-15 14:01:45 -0400

    Wrote problem 3b. It works. Next: tweaking various things.

--- set1/calculations_and_plots.py
+++ set1/calculations_and_plots.py
@@ -3,3 +3,36 @@ Module that uses function_definitions to make plots and do calculations.
 
 """
 
+from __future__ import division
+
+import matplotlib.pyplot as plt
+from scipy.constants import c
+
+from function_definitions import *
+
+# Some constants
+
+T_eff = 5780 # kelvin; similar to the Sun
+tau_array = [3, 1, 0.3, 0.1]
+
+frequency_array = np.logspace(12, 17) # In units of Hz. 
+wavelength_array = c / frequency_array #Spans a wavelength range <10nm to >100um
+
+for tau in tau_array:
+
+    flux_array = np.zeros_like(frequency_array)
+    for i, nu in zip( range(len(flux_array)), frequency_array):
+
+        flux_array[i] = flux(tau, nu, T_eff)
+
+    plt.plot(wavelength_array, flux_array*frequency_array, label=r"$\tau$ = %s" % tau)
+
+
+plt.xlabel("Wavelength (meters)")
+plt.ylabel("Flux * frequency")
+
+plt.title("Problem Set 1, problem #3b")
+
+plt.legend(loc = 'lower right')
+plt.loglog()
+plt.show()
