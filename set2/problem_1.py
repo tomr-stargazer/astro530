dfebe702c79acef1248c0930d97cfe35873ed141
Tom Rice <t.rice90@gmail.com>
2013-10-01 22:25:57 -0400

    I have a sort-of sane behaving saha equation function.

--- set2/problem_1.py
+++ set2/problem_1.py
@@ -12,6 +12,13 @@ https://en.wikipedia.org/wiki/Saha_ionization_equation
 
 """
 
+from __future__ import division
+
+import numpy as np
+
+import astropy.constants as const
+
+
 Model_1 = {'T_eff': 4500,
            'log g': 4.0,
            'T': 4469.7,
@@ -29,3 +36,41 @@ Model_3 = {'T_eff': 15000,
            'T': 14630,
            'n_e': 3.17e14,
            'n_H': 3.67e14}
+
+
+def saha_equation(degeneracy_of_lower_state,
+                  degeneracy_of_upper_state,
+                  ionization_energy_of_lower_state,
+                  ionization_energy_of_upper_state,
+                  temperature,
+                  units_of_energy='eV'):
+    """
+    Calculates the Saha equation for a given set of parameters.
+
+    Returns
+    -------
+    saha_parameter : float
+        The ratio n_{i+1} * n_e / n_i, the output of the Saha equation.
+
+    """
+
+    g_i = degeneracy_of_lower_state
+    g_ip1 = degeneracy_of_upper_state
+
+    epsilon_i = ionization_energy_of_lower_state
+    epsilon_ip1 = ionization_energy_of_upper_state
+
+    T = temperature
+
+    h = const.h.to(units_of_energy+' s')
+    k_B = const.k_B.to(units_of_energy+' / (K)')
+
+    lambda_constant = np.sqrt(h**2 /
+                              (2 * np.pi * const.m_e * k_B * T) )
+
+    saha_parameter = ( 2 / lambda_constant**3 *
+                       g_ip1 / g_i *
+                       np.exp(-(epsilon_ip1 - epsilon_i) /(k_B * T))
+                       )
+
+    return saha_parameter
