7730a3f76fe91298228dd614ea626985521c29dd
Tom Rice <t.rice90@gmail.com>
2013-10-01 21:38:28 -0400

    Wrote a rough docstring for problem_1.

---
+++ set2/problem_1.py
@@ -0,0 +1,13 @@
+"""
+Code relating to problem 1 in Astro 530 HW #2.
+
+Problem 1 is about the Saha equation and ions of various atoms
+in three different stellar atmospheres.
+
+The Saha equation:
+https://en.wikipedia.org/wiki/Saha_ionization_equation
+
+..math::
+\frac{n_{i+1}n_e}{n_i} = \frac{2}{\Lambda^{3}}\frac{g_{i+1}}{g_i}\exp\left[-\frac{(\epsilon_{i+1}-\epsilon_i)}{k_BT}\right]
+
+"""
