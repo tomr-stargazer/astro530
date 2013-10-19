7feaf3144fa09c2ecc74a93a507861948a30b38f
Tom Rice <t.rice90@gmail.com>
2013-10-19 19:12:29 -0400

    I wrote the hjerting approximations!

--- set3/function_definitions_hw3.py
+++ set3/function_definitions_hw3.py
@@ -36,3 +36,51 @@ def u_function(frequency, central_frequency, doppler_width):
     delta_nu = doppler_width
 
     return (nu - nu_0) / delta_nu
+
+def hjerting_gaussian_approximation(damping_parameter, u):
+    """
+    Calculates the H(0, u) Hjerting Gaussian approximation.
+
+    """
+
+    return np.exp(-u**2)
+
+
+def hjerting_H1_approximation(damping_parameter, u):
+    """
+    Calculates the H_1 polynomial Hjerting function approximation.
+
+    """
+
+    a = damping_parameter
+
+    # Two constants from the homework
+    c1 = 0.56491
+    c2 = 0.846
+
+    return a * (c1/u**2 + c2/u**4)
+    
+
+def hjerting_piecewise_approximation(damping_parameter, u):
+    """
+    Approximates the Hjerting function correctly, everywhere.
+
+    At small |u|, it uses the Gaussian approximation; 
+    at larger |u| it uses the H_1 approximation plus the Gaussian approximation.
+
+    We are doing a piecewise approximation to avoid the behavior of
+    H_1 near u=0.
+
+    """
+
+    if damping_parameter > 5e-3:
+        raise ValueError(
+            "This piecewise approximation breaks for large values of `a`!")
+    
+    a = damping_parameter
+
+    if np.abs(u) <= 2:
+        return hjerting_gaussian_approximation(a, u)
+    else:
+        return (hjerting_gaussian_approximation(a, u) + 
+                hjerting_H1_approximation(a, u))
