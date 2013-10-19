bad58a96cc7d9af2dc635cf6de57e65a83d5d3de
Tom Rice <t.rice90@gmail.com>
2013-10-19 19:26:43 -0400

    Wrote test_hjerting, which enabled me to catch a typo!!
    I had transcribed c1 incorrectly.

--- set3/test_function_definitions_hw3.py
+++ set3/test_function_definitions_hw3.py
@@ -10,3 +10,17 @@ def test_u_function():
     assert u_function(nu, nu_0, delta_nu) == -10
 
     
+def test_hjerting():
+
+    assert hjerting_gaussian_approximation(100, 5) == np.exp(-5**2)
+
+    assert (hjerting_H1_approximation(1e-3, 5) == 
+            (1e-3 * (0.56419 / 5**2 + 0.846 / 5**4)) )
+
+    assert (hjerting_piecewise_approximation(1e-3, 1) == 
+            hjerting_gaussian_approximation(100, 1))
+
+    assert (hjerting_piecewise_approximation(1e-3, 5) ==
+            hjerting_H1_approximation(1e-3, 5) + 
+            hjerting_gaussian_approximation(1e-3, 5))
+    
