c4de06f6debf34a30ff8ba21b145750230243b43
Tom Rice <t.rice90@gmail.com>
2013-10-19 23:59:19 -0400

    I wrote problem_1c()! It's got a break statement in it right now
    but mostly works otherwise.
    
    DAMN slow, though. Maybe the vectorize is slow?

--- set3/problem_1.py
+++ set3/problem_1.py
@@ -19,8 +19,6 @@ from function_definitions_hw3 import *
 line_center_wavelength = Quantity(5000, 'Angstrom')
 line_center_frequency = const.c/line_center_wavelength
 
-print line_center_frequency.to('Hz')
-
 def problem_1a():
     """
     Calculate the emergent flux at 5000 Angstroms (0.5 microns) for
@@ -62,3 +60,54 @@ def problem_1b():
     plt.show()
 
     return fig
+
+def problem_1c():
+    """
+    Plot the line profile for a diversity of different zetas.
+
+    Do it once for zero optical depth and once for high optical depth.
+
+    """
+
+    fig1 = plt.figure()
+    fig2 = plt.figure()
+
+    zeta_list = [0.1, 1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e8]
+    a = 1e-4
+    T_eff = 5780
+
+    delta_nu = 4e9 # corresponding to v_thermal = 2 km/s    
+
+    vflux = np.vectorize(flux)
+    vflux_with_line = np.vectorize(flux_with_line)
+
+    nu_array = np.linspace(5.995, 6.005, 500)*1e14
+
+    tau_c_list = [0, 10]
+
+    for tau_c, fig in zip(tau_c_list, (fig1, fig2)):
+        for zeta in zeta_list:
+
+            ax = fig.add_subplot(1,1,1)
+            
+            ratio_array = (vflux_with_line(tau_c, nu_array, T_eff, zeta) /
+                           vflux(tau_c, nu_array, T_eff) )
+
+            ax.plot(nu_array, ratio_array, label=r"$\zeta = 10^{%d}$" % 
+                    np.log10(zeta))
+
+            print "done with zeta = %f" % zeta
+
+            break
+
+        plt.title(r"HW #3, Problem 1c: Line Profiles at $\tau_c = %d$ star of $T_{eff} = 5780 K$. Tom Rice." % tau_c)
+        plt.xlabel(r"Frequency $\nu$ (Hz)")
+        plt.ylabel(r"$\frac{F_\nu}{F_c}$")
+
+        plt.legend()
+        plt.show()
+
+        plt.xlim(nu_array.min(), nu_array.max())
+
+    return (fig1, fig2)
+
