d4d8ab2341e09324a5841ab9a0e20d1f6d041e4b
Tom Rice <t.rice90@gmail.com>
2013-12-02 06:05:15 -0500

    Wrote a basic test for the soundspeed calculation.

---
+++ set5/test_problem_1.py
@@ -0,0 +1,19 @@
+"""
+Some basic unit tests for Problem 1.
+
+"""
+
+from problem_1 import *
+
+def test_sound_speed():
+
+    by_hand_calculation_of_cs_for_T100_mu1 = 908.02
+    
+    assert (round(sound_speed(100,1), -1) ==
+            round(by_hand_calculation_of_cs_for_T100_mu1, -1) )
+
+    cs_test2 = np.sqrt(k_B * 50 / (2*m_p))
+
+    assert sound_speed(50,2).value == cs_test2
+
+    
