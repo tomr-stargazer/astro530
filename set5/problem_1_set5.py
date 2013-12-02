eb25ecd2359069e2deac44f905396457fd05d505
Tom Rice <t.rice90@gmail.com>
2013-12-02 06:31:35 -0500

    Wrote the centrifugal_radius function. I think it works and is sane.

--- set5/problem_1_set5.py
+++ set5/problem_1_set5.py
@@ -10,7 +10,7 @@ from __future__ import division
 
 import numpy as np
 
-from astropy.constants import k_B, m_p
+from astropy.constants import k_B, m_p, G, M_sun
 from astropy.units.quantity import Quantity
 
 def sound_speed(temperature, mean_molecular_weight=1.4):
@@ -41,3 +41,39 @@ def sound_speed(temperature, mean_molecular_weight=1.4):
     
     return c_s.to('m/s')
     
+def centrifugal_radius(time, temperature, angular_velocity_factor):
+    """
+    Computes the centrifugal radius $r_c$ of the infalling envelope.
+
+    r_c(t) = c_s t^3 / 16 * \Omega_0^2
+
+    Parameters
+    ----------
+    time : float
+        Time coordinate (t) in seconds.
+    temperature : float
+        Temperature (T) of the envelope in kelvin.
+    angular_velocity_factor : float
+        Multiplier on the breakup angular frequency (\Omega_b) 
+        of the rotation.
+
+    Returns
+    -------
+    r_c : float
+        Centrifugal radius in meters.
+
+    """
+
+    t = Quantity(time, unit='s')
+    T = temperature
+
+    c_s = sound_speed(T)
+
+    r_o = 0.5 * G * M_sun / c_s**2
+    angular_velocity = angular_velocity_factor * (G * M_sun / r_o**3)**(1/2)
+    
+    omega = Quantity(angular_velocity, unit='s-1')
+
+    r_c = c_s * t**3 / 16 * omega**2
+
+    return r_c.to('m')
