49f0cdd490fe04e146abb32125c94bf3cd711b7a
Tom Rice <t.rice90@gmail.com>
2013-12-03 19:02:10 -0500

    Wrote most of the functions. Mostly untested (but it compiles.)

--- set5/problem_3_set5.py
+++ set5/problem_3_set5.py
@@ -11,6 +11,9 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+from astropy.units.quantity import Quantity
+from astropy.constants import M_sun, G
+
 from problem_1_set5 import sound_speed
 
 def temperature_at_R(radius):
@@ -21,31 +24,83 @@ def temperature_at_R(radius):
 
     R = radius
 
-    return 300 * (R)^(-1/2)
+    return 300 * (R)**(-1/2)
 
-def surface_density_at_R(radius):
+def angular_velocity_at_R(radius, central_mass=1):
     """
-    Returns the surface density at radius R in AU.
+    Returns the angular velocity at radius R in AU.
 
     """
 
-    pass
+    R = Quantity(radius, unit='AU')
+    M = central_mass*M_sun
 
-def t_V_at_t(time):
+    omega = (G * M / R**3)**(1/2)
+
+    return omega
+    
+
+def viscosity_at_R(radius, alpha=0.01):
+    """
+    Returns the viscosity nu at radius R.
+    
+    """
+    
+    R = radius
+    T = temperature_at_R(R)
+    c_s = sound_speed(T)
+    omega = angular_velocity_at_R(R)
+
+    nu = alpha*c_s**2 / omega
+
+    return nu
+
+def t_V_at_t(time, scaling_radius=10):
     """
     Returns the quantity t_V at time t.
 
     """
 
-    pass
+    R_1 = Quantity(scaling_radius, unit='AU')
 
-def Mdot_at_t_R(time, radius):
+    t = time
+
+    t_V = t * 3*viscosity_at_R(R_1) / R_1**2 + 1
+
+    return t_V
+    
+def surface_density_at_R_t(radius, time, normalization_constant=1,
+                           scaling_radius=10):
+    """
+    Returns the surface density at radius R in AU.
+
+    """
+
+    R = radius
+    t = time
+    C = normalization_constant
+    R_1 = Quantity(scaling_radius, unit='AU')
+    nu_R1 = viscosity_at_R(scaling_radius)
+
+    t_V = t_V_at_t(time)
+
+    return C * (t_V)**(-3/2)*R_1 / (3*np.pi*R*nu_R1) * np.exp(-R*R_1/t_V)
+
+def Mdot_at_t_R(time, radius, normalization_constant=1,
+                scaling_radius=10):
     """
     Returns the accretion rate Mdot at time t and radius R.
 
     """
 
-    pass
+    C = normalization_constant
+    t = time
+    R = radius
+    R_1 = scaling_radius
+
+    t_V = t_V_at_t(time)
+    
+    Mdot = C * t_V * np.exp(-R/(R_1 * t_V))*(1 - 2*R/(R_1 * t_V))
 
 def problem_3b():
     """
@@ -68,4 +123,6 @@ def problem_3b():
     
     """
 
+
+    
     
